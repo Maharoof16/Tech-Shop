31931349b5470ab1b6976a8588a9e0335619c357
MAHAROOF KP <maharoofkp16@gmail.com>
2024-10-07 12:44:17 +0530

    modal styling and products filter

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,25 +1,82 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import productsData from './Products.json';
 import { Link } from 'react-router-dom';
+import filterData from "./FilterData.json";
+
 
 const AllProducts = () => {
+    const [allProducts] = useState(productsData); 
+    const [products, setProducts] = useState(productsData); 
+    const [selectedBrand, setBrands] = useState([]);
+    const [selectedCategory,setCategories]=useState([]);
 
+    const handleBrand = (brand) => {
+        setBrands((prev) =>
+            prev.includes(brand) ? prev.filter((item) => item !== brand) : [...prev, brand]
+        );
+    };
+
+    const handleCategory=(category)=>{
+        setCategories((prev) =>
+            prev.includes(category) ? prev.filter((item) => item !== category) : [...prev, category]
+        );
+    }
+
+    useEffect(() => {
+        let filteredProducts = allProducts;
+        if (selectedBrand.length > 0) {
+            filteredProducts = filteredProducts.filter((product) =>
+                selectedBrand.includes(product.brand)
+            );
+        }
+        
+        if (selectedCategory.length > 0) {
+            filteredProducts = filteredProducts.filter((product) =>
+                selectedCategory.includes(product.category)
+            );
+        }
+
+        setProducts(filteredProducts);
+    }, [selectedBrand, selectedCategory,allProducts]);
+    
     const getImageSrc = (imagePath) => {
         return require(`${imagePath}`);
     };
+
 
   return (
     <>
      {productsData.length>0?(
           <div className="container-fluid bg-black text-white">
-          <div className="row mx-5 mt-5">
-              <div className="col-md-2 mt-5">
-                  <h5>Filter</h5>
+          <div className="row mt-5 pt-5">
+              <div className="col-md-2 mt-5 " >
+              <div className="bg-dark text-light p-3 overflow-auto" style={{ maxHeight: "70vh" }}>
+              <h3 className="text-white">Sort By</h3>
+              {filterData.sortMenu.map((item)=>(
+                <p>{item.title}</p>
+              ))}
+        <h3 className="text-white">Brands</h3>
+
+        {filterData.brandsMenu.map((product)=>(
+            <div className="form-check mb-2" key={product.id}>
+            <input className="form-check-input" type="checkbox" id={product.label} checked={selectedBrand.includes(product.label)} onChange={()=>handleBrand(product.label)} />
+            <label className="form-check-label" htmlFor={product.label}>{product.label}</label>
+          </div>
+        ))}
+
+        <h3 className="text-white">Categories</h3>
+        {filterData.categoryMenu.map((product)=>(
+            <div className="form-check mb-2" key={product.id}>
+            <input className="form-check-input" type="checkbox" id={product.label} checked={selectedCategory.includes(product.label)} onChange={()=>handleCategory(product.label)}/>
+            <label className="form-check-label" htmlFor={product.label}>{product.label}</label>
+          </div>
+        ))}
+                </div>  
               </div>
-              <div className="col-md-10 mt-5">
+              <div className="col-md-10 mt-5" >
                   <div className="row">
-                      {productsData.map(product => (
-                          <div className="col-md-4" key={product.id}>
+                      {products.map(product => (
+                          <div className="col-md-3" key={product.id}>
                               <div className="card mb-4 bg-black text-white border-light ">
                               <Link to={`/products/${product.id}`}>
                                   <img src={getImageSrc(product.images[0])} className="card-img-top" alt={product.title} />
